2fdd643d4ca6e831e90ca478a924dd34d94a1ca6
Dmitriy Afanasev <afanasev@comefoto.ru>
2014-10-09 19:30:12 +0700

    Первый коммит проекта plottex.ru

---
+++ protected/yiic.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once(dirname(__FILE__).'/config/project_root_config.php');
+
+// change the following paths if necessary
+$yiic=dirname(__FILE__).'/../'. FRAMEWORK_LOCATION.'yiic.php';
+$config=dirname(__FILE__).'/config/console.php';
+
+require_once($yiic);
